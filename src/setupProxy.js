cc25c51504b621df317f0e4facfd6b7dab8a2f8d
anandissac <anand.z.issac@gmail.com>
2020-05-15 20:57:19 -0400

    Setting up proxies

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        "/services/search/FindingService/v1",
+        createProxyMiddleware({
+            target: "https://svcs.ebay.com",
+            changeOrigin: true
+        }
+
+        )
+    );
+};
